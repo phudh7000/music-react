6b39c38206d7688d0d4dd3a27203b69fe73e8670
phudh7000 <78036536+phudh7000@users.noreply.github.com>
2021-07-08 22:59:51 +0700

    update

--- src/App.js
+++ src/App.js
@@ -13,25 +13,26 @@ import store from './reducers'
 class App extends Component {
 
     renderView(){
-        if(isMobile)return(
-            <Provider store={store}>
+        // if(isMobile)return(
+        //     <Provider store={store}>
                 
-                    <div className="wrapper" style={{ width: "400px", overflowX: "scroll" }}>
-                        <div className="slider">
-                            <div className="side-left">
-                                <SongPlaying />
-                                <SongList />
-                            </div>
+        //             <div className="wrapper" style={{ width: "400px", overflowX: "scroll" }}>
+        //                 <div className="slider">
+        //                     <div className="side-left">
+        //                         <SongPlaying />
+        //                         <SongList />
+        //                     </div>
 
-                            <div className="side-right">
-                                <KindOfMusic />
-                            </div>
-                        </div>
-                    <ButtonControl />
-                    </div>
-            </Provider>
-        )
-        else return(
+        //                     <div className="side-right">
+        //                         <KindOfMusic />
+        //                     </div>
+        //                 </div>
+        //             <ButtonControl />
+        //             </div>
+        //     </Provider>
+        // )
+        // else 
+        return(
             <Provider store={store}>
                     <div className="wrapper">
                             <div className="side-left">
